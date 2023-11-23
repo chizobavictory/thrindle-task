319bfd01323eec3e8af2782f02f0c8ec0cf57f66
Chizoba Victory <chizoba.victory1@gmail.com>
2023-11-23 20:48:56 +0100

    Refactor paymentController.ts and added exampleenv

--- controller/paymentController.ts
+++ controller/paymentController.ts
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import { Request, Response } from 'express';
-import {config} from "../utils/config"
-const BASE_URL = 'https://api.paystack.co';
+import axios from "axios";
+import { Request, Response } from "express";
+import { config } from "../utils/config";
+const BASE_URL = "https://api.paystack.co";
 
 export const initializePayment = async (req: Request, res: Response) => {
   const { amount, email } = req.body;
 
   const headers = {
-    Accept: 'application/json',
+    Accept: "application/json",
     Authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   };
 
   try {
@@ -24,13 +24,13 @@ export const initializePayment = async (req: Request, res: Response) => {
 
     res.json({
       status: true,
-      message: 'successful transaction',
+      message: "successful transaction",
       data: response.data,
     });
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: 'failed to initialize transaction',
+      message: "failed to initialize transaction",
       data: null,
     });
   }
@@ -40,7 +40,7 @@ export const verifyPayment = async (req: Request, res: Response) => {
   const { reference } = req.params;
 
   const headers = {
-    Accept: 'application/json',
+    Accept: "application/json",
     Authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
   };
 
@@ -49,32 +49,47 @@ export const verifyPayment = async (req: Request, res: Response) => {
 
     res.json({
       status: true,
-      message: 'transaction verified',
+      message: "transaction verified",
       data: response.data,
     });
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: 'failed to verify transaction',
+      message: "failed to verify transaction",
       data: null,
     });
   }
 };
 
 export const listTransactions = async (req: Request, res: Response) => {
+  const { startDate, endDate } = req.query; // Assuming you want to search by date range
+
   const headers = {
-    Accept: 'application/json',
+    Accept: "application/json",
     Authorization: `Bearer ${config.PAYSTACK_SECRET_KEY}`,
   };
 
   try {
-    const response = await axios.get(`${BASE_URL}/transaction`, { headers });
+    // Construct the Paystack API URL with optional query parameters
+    const apiUrl = `${BASE_URL}/transaction?`;
+
+    // Add query parameters based on user input
+    const queryParams: string[] = [];
+    if (startDate) {
+      queryParams.push(`startDate=${startDate}`);
+    }
+    if (endDate) {
+      queryParams.push(`endDate=${endDate}`);
+    }
+
+    // Make the GET request to Paystack
+    const response = await axios.get(apiUrl + queryParams.join("&"), { headers });
 
     res.json(response.data);
   } catch (error) {
     res.status(500).json({
       status: false,
-      message: 'failed to retrieve transactions',
+      message: "failed to retrieve transactions",
       data: null,
     });
   }
---
+++ readme.md
@@ -0,0 +1,21 @@
+### Thrindle Payment API Documentation
+# Overview
+The Thrindle Payment API allows authorized consumers to make money transfers using the Paystack payment gateway. Additionally, users can retrieve and search their transaction history.
+
+# Stack
+- Node.js
+- Express
+- MongoDB
+- Typescript
+- Paysack API
+
+# Authentication
+All payment endpoints require authentication via JSON Web Token (JWT). Include the JWT token in the Authorization header with the format: Bearer <token>.
+
+# Postman Documentation: 
+Every endpoint has the required documentation explained in full on Postman. You can access the documentation via the link below:
+https://www.postman.com/telecoms-explorer-27073331/workspace/chizobavictory/collection/24035086-20581051-9d48-42ac-8e39-4ac8e6713067?action=share&creator=24035086&active-environment=24035086-18db1989-d499-4ff5-aaef-9dd8df35dcc9
+
+# Installation
+- Clone the repository
+- Run npm install to install dependencies
